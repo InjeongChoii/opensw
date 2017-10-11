eef21176b7f78399321adf96523c5612479ab649
InjeongChoii <starec6230@kookmin.ac.kr>
2017-10-12 04:35:03 +0900

    Add a fooditem class and Seperate values to xml files for maintenance

--- Refrigerator/app/src/main/java/org/androidtown/refrigerator/MainActivity.java
+++ Refrigerator/app/src/main/java/org/androidtown/refrigerator/MainActivity.java
@@ -1,9 +1,13 @@
 package org.androidtown.refrigerator;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,16 +27,26 @@ public class MainActivity extends AppCompatActivity {
         listview = (ListView) findViewById(R.id.listview1);
 
         // 첫 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.food_example),
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_local_dining_black_24dp),
                 "food", "08-15") ;
         // 두 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.food_example),
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_local_dining_black_24dp),
                 "food1","10-15") ;
         // 세 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.food_example),
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_local_dining_black_24dp),
                 "food2","11-15") ;
 
         listview.setAdapter(adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegistProduct.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 
