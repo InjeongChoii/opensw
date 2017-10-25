21dca4d50a3bc02eb29c517953c1b0d88ce42fcc
경혜안 <hean1103@naver.com>
2017-10-25 13:07:20 +0900

    Add spinner about food category

--- Refrigerator/app/src/main/java/org/androidtown/refrigerator/RegistProduct.java
+++ Refrigerator/app/src/main/java/org/androidtown/refrigerator/RegistProduct.java
@@ -1,13 +1,57 @@
+
 package org.androidtown.refrigerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class RegistProduct extends AppCompatActivity {
 
+
+ArrayList arraylist;
+    public static String select_item="";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist_product);
+        arraylist = new ArrayList();
+        arraylist.add("과일");
+        arraylist.add("채소");
+        arraylist.add("생선");
+        arraylist.add("해산물");
+        arraylist.add("견과");
+        arraylist.add("쌀/잡곡");
+        arraylist.add("정육");
+        arraylist.add("계란류");
+        arraylist.add("유제품");
+        arraylist.add("음료");
+        arraylist.add("간식");
+        arraylist.add("냉동/간편식품");
+        arraylist.add("통조림");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,arraylist);
+        Spinner sp = (Spinner)findViewById(R.id.type_spinner);
+        sp.setPrompt("유형을 선택하세요.");
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+                public void onItemSelected(AdapterView arg0,View arg1,int arg2,long arg3){
+                    Toast.makeText(getBaseContext(),arraylist.get(arg2)+"이 설정되었습니다.",
+                            Toast.LENGTH_SHORT).show();
+                    select_item = String.valueOf(arraylist.get(arg2));
+                }
+
+            @Override
+            public void onNothingSelected(AdapterView arg0) {
+
+            }
+        });
     }
+
 }
